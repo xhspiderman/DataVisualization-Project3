77147f75452e824d0f14142a204991ceed30580c
agon <agon@hawaii.edu>
2014-04-04 00:57:42 -1000

    Simple mock-up home page showing all characters pictures randomly positioned and with random withs

--- js/code.js
+++ js/code.js
@@ -1,18 +1,52 @@
 //File to hold the main code of the Simpsons Visualization Application
 //authors: Alberto Gonzalez, Hao Xu date:April 2014
 var character_info = [];
+var circle = [] 
 d=20;
+
+var div = d3.select(".home")
+        .append('div')
+        .attr('class', 'canvas')
+        .attr('width', 1280)
+        .attr('height', 720)
+        .attr('class','canvas')
 //Get information for the characters in an array
 for (var i=0;i<character.info.length;i++)
 {
-	character_info.push(character.info[i].Name);
+	r=Math.random()*60;
+	cy=Math.random()*720;
+	while(cy<10) cy=Math.random()*720;
+	while(cy>650) cy=Math.random()*720;
+	cx=Math.random()*1280;
+	character_info.push(character.info[i]);
+    var img = d3.select(".canvas")
+    		.append('img')
+    		.attr('class','image')
+    		.attr('width',r)
+    		.attr('height',r)
+    		.attr('src',character_info[i].image)
+    		.style('top',String(cy)+'px')
+    		.style('right',String(cx)+'px')
+
+	/*circle.push(svg.append('circle')
+		.attr("cy", cy)
+		.attr("cx", cx)
+		.attr("r", r)
+		.attr("class","little")
+		.style("fill", "transparent")       
+	    .style("stroke", "black")     
+	    .style("stroke-width", 0.25)
+	);
+	var defs = svg.append('svg:defs');
+            defs.append('svg:pattern')
+                .attr('id', 'tile-ww')
+                .attr('patternUnits', 'userSpaceOnUse')
+                .attr('width', r)
+                .attr('height', r)
+                .append('svg:image')
+                .attr('src', character_info[i].image)
+                .attr('x', cx-r*0.9)
+                .attr('y', cy-r*0.9)
+                .attr('width', r)
+                .attr('height', r);*/
 }
-circle = d3.select("svg").append('circle')
-	.style("fill", "steelblue")
-	.data(character_info)
-	.enter().append("circle")
-    .attr("cy", 90)
-    .attr("cx", 30)
-    .attr("r", Math.random()*10)
-    .attr("class","little")
-	.style("fill", "steelblue")
