8375b312d41869ca605f9155853fa85678dde1fe
agon <agon@hawaii.edu>
2014-04-03 18:58:40 -1000

    output_character.txt holds the values for character name, hair color and url to picture

--- datastriping/characterscript.py
+++ datastriping/characterscript.py
@@ -42,19 +42,19 @@ for source in sources:
 		characterDIV=soup.find(id='mw-content-text').find_all('table')[0].find_all('td')
 		#get table with info
 		for row in characterDIV:
-			#get gender and status
-			if row.find('a'):
-				genderCharacterinfo = str(row.find('a')['href']).lower()
-			else:
-				genderCharacterinfo = "unknown"
-			if "category:male_characters" in genderCharacterinfo:
-				character_gender="male"
-			if "category:female_characters" in genderCharacterinfo:
-				character_gender="female"
-			if "category:alive_characters" in genderCharacterinfo:
-				character_status="alive"
-			if "category:deceased_characters" in genderCharacterinfo:
-				character_status="deceased"
+	#		#get gender and status
+	#		if row.find('a'):
+	#			genderCharacterinfo = str(row.find('a')['href']).lower()
+	#		else:
+	#			genderCharacterinfo = "unknown"
+	#		if "category:male_characters" in genderCharacterinfo:
+	#			character_gender="male"
+	#		if "category:female_characters" in genderCharacterinfo:
+	#			character_gender="female"
+	#		if "category:alive_characters" in genderCharacterinfo:
+	#			character_status="alive"
+	#		if "category:deceased_characters" in genderCharacterinfo:
+	#			character_status="deceased"
 			#get rest of attributes
 			if row.find_all('b'):
 				moreCharacterinfoDIV= row.find_all('b')
@@ -62,21 +62,23 @@ for source in sources:
 					#get hair color
 					if "Hair" in element:
 						character_hair=str(row.find_next_sibling()).split('<td>')[1].split('</td>')[0].split('\n')[0]
-					#get character occupation
-					if "Occupation" in element:
-						#pdb.set_trace()
-						character_occupation=str(row.find_next_sibling()).split('<td>')[1].split('</td>')[0].split('\n')[0]
-					if "Voiced By" in element:
-						character_voicedBy=str(row.find_next_sibling().get_text()).split('\n')[0]
+	#				#get character occupation
+	#				if "Occupation" in element:
+	#					#pdb.set_trace()
+	#					character_occupation=str(row.find_next_sibling()).split('<td>')[1].split('</td>')[0].split('\n')[0]
+	#				if "Voiced By" in element:
+	#					#pdb.set_trace()
+	#					character_voicedBy=str(row.find_next_sibling().get_text()).split('\n')[0]
 	else:
 		character_gender ="unknown"
-	print(character_gender+ " "+character_status+ character_hair + " "+ character_occupation+ " "+ character_voicedBy)
+	print(character+ " " + character_hair + " " + image)
 	# outtput the result as one to one data format into result file
 	resultFile.write('"'+character.encode('UTF-8')+'"'+'   ')
-	resultFile.write('"'+character_gender.encode('UTF-8')+'"'+'   ')
-	resultFile.write('"'+character_status.encode('UTF-8')+'"'+'   ')
+	#resultFile.write('"'+character_gender.encode('UTF-8')+'"'+'   ')
+	#resultFile.write('"'+character_status.encode('UTF-8')+'"'+'   ')
 	resultFile.write('"'+character_hair.encode('UTF-8')+'"'+'   ')
-	resultFile.write('"'+character_occupation.encode('UTF-8')+'"'+'   ')
-	#resultFile.write('"'+image.encode('UTF-8')+'"'+'   ')
+	#resultFile.write('"'+character_occupation.encode('UTF-8')+'"'+'   ')
+	#resultFile.write('"'+character_voicedBy.encode('UTF-8')+'"'+'   ')
+	resultFile.write('"'+image.encode('UTF-8')+'"'+'   ')
 	resultFile.write('\n')
 resultFile.close()
--- datastriping/debug.py
+++ datastriping/debug.py
@@ -12,49 +12,39 @@ character=soup.find(id='WikiaPageHeader').find('h1').string
 
 #  get the episodes the character is in
 characterDIV=soup.find(id='mw-content-text').find_all('table')[0].find_all('td')
-for row in characterDIV:
-	if row.find('a'):
-		genderCharacterinfo = str(row.find('a')['href']).lower()
-	else:
-		genderCharacterinfo = "unknown"
-	if "category:male_characters" in genderCharacterinfo:
-		print("male")
-	if "category:female_characters" in genderCharacterinfo:
-		print("female")
-	if "category:alive_characters" in genderCharacterinfo:
-		print("alive")
-	if "category:deceased_characters" in genderCharacterinfo:
-		print("dead")
-if characterDIV[7].find_all('b'):
-	moreCharacterinfoDIV= characterDIV[7].find_all('b')
-	for element in moreCharacterinfoDIV:
-		if "hair" in element:
-			pdb.set_trace()
-			hairDIV=characterDIV[7].find_next_sibling('td')
-#character_info=checkDIV.replace("\n", "")
-#gender=character_info.split('gender')
-#print((checkDIV.encode('UTF-8')).split('td'))
-pdb.set_trace()
-if checkDIV:
-	#pdb.set_trace()
-	episodesDIV=checkDIV.find_parent().find_next_sibling('table')
+#get character info
+if soup.find(id='mw-content-text').find_all('table'):
+	characterDIV=soup.find(id='mw-content-text').find_all('table')[0].find_all('td')
+	#get table with info
+	for row in characterDIV:
+		#get gender and status
+		if row.find('a'):
+			genderCharacterinfo = str(row.find('a')['href']).lower()
+		else:
+			genderCharacterinfo = "unknown"
+		if "category:male_characters" in genderCharacterinfo:
+			character_gender="male"
+		if "category:female_characters" in genderCharacterinfo:
+			character_gender="female"
+		if "category:alive_characters" in genderCharacterinfo:
+			character_status="alive"
+		if "category:deceased_characters" in genderCharacterinfo:
+			character_status="deceased"
+		#get rest of attributes
+		if row.find_all('b'):
+			moreCharacterinfoDIV= row.find_all('b')
+			for element in moreCharacterinfoDIV:
+				#get hair color
+				if "Hair" in element:
+					character_hair=str(row.find_next_sibling()).split('<td>')[1].split('</td>')[0].split('\n')[0]
+				#get character occupation
+				if "Occupation" in element:
+					pdb.set_trace()
+					character_occupation=str(row.find_next_sibling()).split('<td>')[1].split('</td>')[0].split('\n')[0]
+				if "Voiced By" in element:
+					#pdb.set_trace()
+					character_voicedBy=str(row.find_next_sibling().get_text()).split('\n')[0]
 else:
-	episodesDIV = None
-
-if episodesDIV:
-	episodes1=episodesDIV.find_all(text="Episode")
-	episodes2=episodesDIV.find_all(text="THOH")
-	episodes = episodes1+episodes2
-	if not episodes:
-		episodes=["none"]
-else:
-	repisodes= ["none"]
-
-# out put the result as one to one data format into result file
-for episode in episodes:
-	if episode!="none":
-	    episode_name = episode.find_parent('i').find_next_sibling('a').string
-	else:
-		episode_name = episode
-
+	character_gender ="unknown"
+	print(character_gender+ " "+character_status+ character_hair + " "+ character_occupation+ " "+ character_voicedBy)
 pdb.set_trace()
